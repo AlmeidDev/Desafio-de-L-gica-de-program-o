f167bd654649edad307afac6d418c0ec2f62deb3
AlmeidDev <felipealmeidadev04@gmail.com>
2024-08-18 10:21:47 -0300

    let nomesPokemon = [     ["POOCHYENA", 2 , "M ", "13/13"],     ["ZIGZAGOON", 2 , "F ", "13/13"],     ["DRAGONITE", 5 , "M ", "25/25"],     ["DRAGONITE", 5 , "F", "24/24"],     ["DRAGONITE", 5 , "F", "24/24"],     ["POOCHYENA", 3 , "F", "15/15"],     ["WURMPLE", 2 ,"M","7/14"]     ];     const infor =[         ["Pokemon:"],         ["Level:"],         ["Sexo:"],         ["HP:"],         ["Foi cadastrado com sucesso"]     ];
        console.log(infor [0]+ nomesPokemon [0][0],infor[1]+ nomesPokemon[0][1],infor[2]+nomesPokemon[0][2],infor[3]+nomesPokemon[0][3],infor[4][0]),
        console.log(infor[0]+ nomesPokemon [1][0],infor[1]+ nomesPokemon[1][1],infor[2]+nomesPokemon[1][2],infor[3]+nomesPokemon[1][3],infor[4][0]),
        console.log(infor[0]+ nomesPokemon[2][0],infor[1]+ nomesPokemon[2][1],infor[2]+nomesPokemon[2][2],infor[3]+nomesPokemon[2][3],infor[4][0]),
        console.log(infor[0]+nomesPokemon[3][0],infor[1]+nomesPokemon[3][1],infor[2]+nomesPokemon[3][2],infor[3]+nomesPokemon[3][3],infor[4][0]),
        console.log(infor[0]+nomesPokemon[4][0],infor[1]+nomesPokemon[4][1],infor[2]+nomesPokemon[4][2],infor[3]+nomesPokemon[4][3],infor[4][0]),
        console.log(infor[0]+nomesPokemon[5][0],infor[1]+nomesPokemon[5][1],infor[2]+nomesPokemon[5][2],infor[3]+nomesPokemon[5][3],infor[4][0]),
        console.log(infor[0]+nomesPokemon[6][0],infor[1]+nomesPokemon[6][1],infor[2]+nomesPokemon[6][2],infor[3]+nomesPokemon[6][3],infor[4][0])

--- Desafio.js
+++ Desafio.js
@@ -1,12 +1,24 @@
 let nomesPokemon = [
-["POOCHYENA", 2 , "M", "13/13"],
-["ZIGZAGOON", 2 , "F", "13/13"],
-["DRAGONITE", 5 , "M", "25/25"],
-["DRAGONITE", 5 , "F", "24/24"],
-["DRAGONITE", 5 , "F", "24/24"],
-["POOCHYENA", 3 , "F", "15/15"],
-["WURMPLE", 2 ,"M","7/14"]
-];
-const mensagem = " Foi cadastrado com sucesso"
-
-console.log(nomesPokemon [0] [0] + mensagem)
+    ["POOCHYENA", 2 , "M ", "13/13"],
+    ["ZIGZAGOON", 2 , "F ", "13/13"],
+    ["DRAGONITE", 5 , "M ", "25/25"],
+    ["DRAGONITE", 5 , "F", "24/24"],
+    ["DRAGONITE", 5 , "F", "24/24"],
+    ["POOCHYENA", 3 , "F", "15/15"],
+    ["WURMPLE", 2 ,"M","7/14"]
+    ];
+    const infor =[
+        ["Pokemon:"],
+        ["Level:"],
+        ["Sexo:"],
+        ["HP:"],
+        ["Foi cadastrado com sucesso"]
+    ];
+    
+    console.log(infor [0]+ nomesPokemon [0][0],infor[1]+ nomesPokemon[0][1],infor[2]+nomesPokemon[0][2],infor[3]+nomesPokemon[0][3],infor[4][0]),
+    console.log(infor[0]+ nomesPokemon [1][0],infor[1]+ nomesPokemon[1][1],infor[2]+nomesPokemon[1][2],infor[3]+nomesPokemon[1][3],infor[4][0]),
+    console.log(infor[0]+ nomesPokemon[2][0],infor[1]+ nomesPokemon[2][1],infor[2]+nomesPokemon[2][2],infor[3]+nomesPokemon[2][3],infor[4][0]),
+    console.log(infor[0]+nomesPokemon[3][0],infor[1]+nomesPokemon[3][1],infor[2]+nomesPokemon[3][2],infor[3]+nomesPokemon[3][3],infor[4][0]),
+    console.log(infor[0]+nomesPokemon[4][0],infor[1]+nomesPokemon[4][1],infor[2]+nomesPokemon[4][2],infor[3]+nomesPokemon[4][3],infor[4][0]),
+    console.log(infor[0]+nomesPokemon[5][0],infor[1]+nomesPokemon[5][1],infor[2]+nomesPokemon[5][2],infor[3]+nomesPokemon[5][3],infor[4][0]),
+    console.log(infor[0]+nomesPokemon[6][0],infor[1]+nomesPokemon[6][1],infor[2]+nomesPokemon[6][2],infor[3]+nomesPokemon[6][3],infor[4][0])
