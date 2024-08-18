07bc1bccebfbcab54813d73a8652cf5c8b70ef33
AlmeidDev <felipealmeidadev04@gmail.com>
2024-08-17 21:40:16 -0300

    first commit

---
+++ Desafio.js
@@ -0,0 +1,12 @@
+let nomesPokemon = [
+["POOCHYENA", 2 , "M", "13/13"],
+["ZIGZAGOON", 2 , "F", "13/13"],
+["DRAGONITE", 5 , "M", "25/25"],
+["DRAGONITE", 5 , "F", "24/24"],
+["DRAGONITE", 5 , "F", "24/24"],
+["POOCHYENA", 3 , "F", "15/15"],
+["WURMPLE", 2 ,"M","7/14"]
+];
+const mensagem = " Foi cadastrado com sucesso"
+
+console.log(nomesPokemon [0] [0] + mensagem)
